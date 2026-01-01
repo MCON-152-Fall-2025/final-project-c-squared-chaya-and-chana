85f0b71541aa96feaf4fe732e15b75e356b361fe
mussygee <cgoldste9@student.touro.edu>
2026-01-01 16:15:49 -0500

    RSF2-218: add servings ascending recipe sort strategy

---
+++ recipeshare/src/main/java/com/mcon152/recipeshare/service/ServingsAscendingSortStrategy.java
@@ -0,0 +1,18 @@
+package com.mcon152.recipeshare.service;
+
+import com.mcon152.recipeshare.domain.Recipe;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ServingsAscendingSortStrategy implements RecipeSortStrategy {
+
+    @Override
+    public List<Recipe> sort(List<Recipe> recipes) {
+        List<Recipe> sorted = new ArrayList<>(recipes);
+        sorted.sort(Comparator.comparing(Recipe::getServings));
+        return sorted;
+    }
+}
+
